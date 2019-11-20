dba8f3d548738a7a2b6c6ad9f6927bdeadf78a92
Jordan Martinez <jordanalex.martinez@gmail.com>
2019-11-19 21:48:45 -0800

    Fix compiler error: import function aliases in Data.Tree

--- src/Data/Tree.purs
+++ src/Data/Tree.purs
@@ -2,7 +2,7 @@ module Ocelot.Data.Tree where
 
 import Prelude
 
-import Data.Argonaut (class DecodeJson, decodeJson, (.?=), (.??))
+import Data.Argonaut (class DecodeJson, decodeJson, (.!=), (.:?))
 import Data.Function (on)
 import Data.Lens (Lens')
 import Data.Lens.Iso.Newtype (_Newtype)
