12b6b0d09d529540f994613d6b5985a695f98e76
Serhii Khoma <srghma@gmail.com>
2020-08-06 18:48:11 +0300

    feat: refactor: css generator

--- css/postcss.config.js
+++ css/postcss.config.js
@@ -1,23 +1,6 @@
 module.exports = scoped => {
-  if (scoped) {
-    return [
-      require('tailwindcss')('./tailwind.config.js'),
-      require('postcss-prefix-selector')({
-        prefix: '.ocelot-scoped',
-        transform(prefix, selector, prefixedSelector) {
-          if (selector === 'html') {
-            return `html, ${prefix}`
-          }
-          if (selector === 'body') {
-            return `body ${prefix}`
-          }
-          return prefixedSelector
-        }
-      }),
-    ]
-  }
-
   return [
-    require('tailwindcss')('./tailwind.config.js'),
+    require('tailwindcss'),
+    require('autoprefixer'),
   ]
 }
